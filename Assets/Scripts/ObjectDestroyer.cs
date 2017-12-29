1d9dbbef27941de30365aa358422568ba8884121
Rodrigo Januário da Silva <rjdsilv@gmail.com>
2017-12-28 21:28:49 -0500

    Added the object catcher.

---
+++ Assets/Scripts/ObjectDestroyer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Class responsible for destroying objects that leaves the game area.
+/// </summary>
+public class ObjectDestroyer : MonoBehaviour
+{
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
