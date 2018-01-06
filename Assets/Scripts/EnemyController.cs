d8885231ad973fd094814ab32293c157289a12f2
Rodrigo Januário da Silva <rjdsilv@gmail.com>
2018-01-06 18:37:26 -0500

    Programmed the Boss shoting pattern

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -14,6 +14,7 @@ public class EnemyController : MonoBehaviour
     public int formationSize;           // The number of enemies in the formation.
     public bool spawnInFormations;      // Indicates if the enemy will spawn in formation.
     public bool shotBack;               // Indicates if the enemy shots back at the player.
+    public bool isBoss;                 // Indicates if the enemy is a boss;
     public float nextEnemyWait;         // The wait for spawning the next enemy in the formation.
     public float shotInterval;          // The enemy shot interval, if it shots back.
     public float shotBackSpeed;         // The speed that the enemy shot will travel on the screen.
@@ -21,12 +22,16 @@ public class EnemyController : MonoBehaviour
     public GameObject enemyObject;      // The enemy object to be controlled.
     public GameObject enemyShot;        // The enemy shot object that will be used.
 
+    // Script private variables.
+    private static bool hasShield = true;
+
     /// <summary>
     /// Spawns the enemies based on the properties given by the programmer on unit's screen.
     /// </summary>
     /// <returns>The IEnumerator so this method can be used as a coroutine.</returns>
     public IEnumerator Spawn(float xPosition, float yPosition)
     {
+        hasShield = true;
         GameObject[] enemies = spawnInFormations ? new GameObject[formationSize] : new GameObject[1];
 
         // Spawn the enemies
@@ -50,30 +55,101 @@ public class EnemyController : MonoBehaviour
         // Makes the enemies shot back.
         if (shotBack)
         {
+            // Gets the player on the scene.
+            GameObject ship = GameObject.FindGameObjectWithTag("Player");
+
             while (AreEnemiesAlive(enemies))
             {
                 foreach (GameObject enemy in enemies)
                 {
-                    // Gets the player on the scene.
-                    GameObject ship = GameObject.FindGameObjectWithTag("Player");
-
-                    // The player is still alive?
-                    try
+                    if (!isBoss)
                     {
-                        if (null != ship)
+                        EnemyShot(ship, enemy);
+                        yield return new WaitForSeconds(shotInterval);
+                    }
+                    else
+                    {
+                        if (!HasShield(enemy))
                         {
-                            Vector3 shotDirection = ship.transform.position - enemy.transform.position;
-                            GameObject shot = Instantiate(enemyShot, enemy.transform.GetChild(0).position, Quaternion.identity);
-                            shot.GetComponent<Rigidbody2D>().velocity = shotDirection.normalized * shotBackSpeed;
+                            yield return BossShot(ship, enemy, 0, 4, false);
+                            yield return new WaitForSeconds(shotInterval);
+                            yield return BossShot(ship, enemy, 4, 10, false);
+                            yield return new WaitForSeconds(shotInterval);
+                            yield return BossShot(ship, enemy, 0, 10, true);
+                            yield return new WaitForSeconds(shotInterval);
+                            yield return BossShot(ship, enemy, 0, 10, false);
+                            yield return new WaitForSeconds(shotInterval);
+                        }
+                        else
+                        {
+                            yield return new WaitForSeconds(shotInterval / 2);
                         }
                     }
-                    catch (Exception)
-                    {
-                    }
-
-                    yield return new WaitForSeconds(shotInterval);
                 }
             }
+        }
+    }
+
+    private static bool HasShield(GameObject enemy)
+    {
+        if (hasShield)
+        {
+            foreach (Transform spawner in enemy.transform)
+            {
+                if (spawner.tag == "Shield")
+                {
+                    return true;
+                }
+            }
+
+            hasShield = false;
+        }
+
+        return hasShield;
+    }
+
+    private IEnumerator BossShot(GameObject ship, GameObject enemy, int fromWeapon, int toWeapon, bool waitNextShot)
+    {
+        for (int i = fromWeapon; i < toWeapon; i++)
+        {
+            try
+            {
+                if (null != ship)
+                {
+                    Transform currChild = enemy.transform.GetChild(i);
+                    if (currChild.tag != "Shield")
+                    {
+                        Vector3 shotDirection = ship.transform.position - currChild.position;
+                        GameObject shot = Instantiate(enemyShot, currChild.position, Quaternion.identity);
+                        shot.GetComponent<Rigidbody2D>().velocity = shotDirection.normalized * shotBackSpeed;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            if (waitNextShot)
+            {
+                yield return new WaitForSeconds(shotInterval / 2);
+            }
+        }
+    }
+
+    private void EnemyShot(GameObject ship, GameObject enemy)
+    {
+        try
+        {
+            // The player is still alive?
+            if (null != ship)
+            {
+                Vector3 shotDirection = ship.transform.position - enemy.transform.position;
+                GameObject shot = Instantiate(enemyShot, enemy.transform.GetChild(0).position, Quaternion.identity);
+                shot.GetComponent<Rigidbody2D>().velocity = shotDirection.normalized * shotBackSpeed;
+            }
+        }
+        catch (Exception)
+        {
         }
     }
 
