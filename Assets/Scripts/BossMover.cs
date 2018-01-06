156205231ff1d99bc6a5290397c0625996d4eccc
Rodrigo Januário da Silva <rjdsilv@gmail.com>
2018-01-06 17:44:39 -0500

    Created more enemies

---
+++ Assets/Scripts/BossMover.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class BossMover : MonoBehaviour
+{
+    public int xSpeed;
+    public float stopPosition;
+
+    // The boss Rigid Body
+    private Rigidbody2D rBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    /// <summary>
+    /// Makes the boss movement calculation.
+    /// </summary>
+    void FixedUpdate()
+    {
+        if (stopPosition < transform.position.x)
+        {
+            rBody.velocity = xSpeed * Vector2.left;
+        }
+        else
+        {
+            rBody.velocity = Vector2.zero;
+            foreach(Transform child in transform)
+            {
+                if (child.tag == "Shield")
+                {
+                    Destroy(child.gameObject);
+                }
+            }
+        }
+    }
+}
